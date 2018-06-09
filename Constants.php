c0e5dfffc71db843318fcc81780bbd4e4f1e8397
sleekframework <sleekphp@gmail.com>
2018-06-09 23:40:40 +0530

    Adds Database and Web root constants

--- Constants.php
+++ Constants.php
@@ -8,6 +8,72 @@
  * ----------------------------------------------------------------------
  */
 
+/**----------------------------------------------------------------------
+ *                  DATABASE CONSTANTS
+ * ----------------------------------------------------------------------
+ */
 
+/**
+ * Constant for DATABASE SERVER
+ * @var string 
+ */
+defined('DB_SERVER') ? NULL : define('DB_SERVER', 'localhost');
+
+/**
+ * Constant for DATABASE PORT
+ * @var integer 
+ */
+defined('DB_PORT') ? NULL : define('DB_PORT', 8889);
+
+/**
+ * Constant for DATABASE USER
+ * @var string 
+ */
+defined('DB_USER') ? NULL : define('DB_USER', 'root');
+
+/**
+ * Constant for DATABASE PASSWORD
+ * @var string 
+ */
+defined('DB_PASSWORD') ? NULL : define('DB_PASSWORD', 'root');
+
+/**
+ * Constant for DATABASE NAME
+ * @var string 
+ */
+defined('DB_NAME') ? NULL : define('DB_NAME', 'sleek');
+
+
+/**
+ * --------------------------------------------------------------------------
+ *                    website root constants
+ * --------------------------------------------------------------------------
+ */
+
+/**
+ * Directory separator
+ */
+define('DS', DIRECTORY_SEPARATOR);
+
+/**
+ * Web ROOT directory
+ */
+define('SITE_ROOT', __DIR__);
+
+
+/**
+ * Controllers Directory path
+ */
+define('CONTROLLERS', SITE_ROOT.DS.'src'.DS.'Controllers');
+
+/**
+ * Models Directory Path
+ */
+define('MODELS', SITE_ROOT.DS.'src'.DS.'Entity');
+
+/**
+ * Views Directory path
+ */
+define('VIEWS', SITE_ROOT.DS.'resources'.DS.'Views');
 
 
